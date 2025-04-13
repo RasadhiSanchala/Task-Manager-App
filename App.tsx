797e1db1770df93d2df458911ef5c09141e7a15b
Rasadhi Sanchala <rspathirana1017@gmail.com>
2025-04-13 19:34:10 +0530

    Import AddTaskScreen

--- App.tsx
+++ App.tsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { enableScreens } from 'react-native-screens';  
+import { enableScreens } from 'react-native-screens';
 
-import LoadingScreen from './src/screens/loadingScreen';
+import LoadingScreen from './src/screens/LoadingScreen';
 import HomeScreen from './src/screens/HomeScreen';
+import AddTaskScreen from './src/screens/AddTaskScreen';  // Don't forget to import the AddTaskScreen
 
-enableScreens(); 
+enableScreens();
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Loading">
-        <Stack.Screen name="Loading" component={LoadingScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Loading">
+        <Stack.Screen name="Loading" component={LoadingScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="AddTask" component={AddTaskScreen} options={{ headerShown: true }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 };
 
