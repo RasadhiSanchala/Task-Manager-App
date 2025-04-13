4734db91d9b48ec2d6bdfc30b88b399ae03126f4
Rasadhi Sanchala <rspathirana1017@gmail.com>
2025-04-13 12:06:30 +0530

    Import the NavigationContainer, createNativeStackNavigator and the enableScreen for smooth navigation.

--- App.tsx
+++ App.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
-import SplashScreen from './src/screens/loadingScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { enableScreens } from 'react-native-screens';  
+
 import LoadingScreen from './src/screens/loadingScreen';
+import HomeScreen from './src/screens/HomeScreen';
+
+enableScreens(); 
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
-  return <LoadingScreen />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Loading">
+        <Stack.Screen name="Loading" component={LoadingScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
