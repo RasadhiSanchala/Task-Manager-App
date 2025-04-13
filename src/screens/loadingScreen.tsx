d9407d676847e79fb10a11dda37cba47e14524c2
Rasadhi Sanchala <rspathirana1017@gmail.com>
2025-04-13 12:09:31 +0530

    Import the useEffect, useNavigation, NativeStackNavigationProp and the RootStackParamList

--- src/screens/loadingScreen.tsx
+++ src/screens/loadingScreen.tsx
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, ImageBackground, Image } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import styles from '../styles/loadingStyles';
+import { RootStackParamList } from '../types/navigation';
 
 const LoadingScreen = () => {
+  const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigation.navigate('Home'); 
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <ImageBackground
       source={require('../assets/LoadingPage.png')}
       style={styles.background}
     >
       <View style={styles.overlay}>
-        <Image
-          source={require('../assets/logo.png')}
-          style={styles.logo}
-          resizeMode="contain"
-        />
+        <Image source={require('../assets/logo.png')} style={styles.logo} />
       </View>
     </ImageBackground>
   );
