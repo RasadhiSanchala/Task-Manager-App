b05b304e384e3bff8413dcfd0035babfcc4e0a7a
Rasadhi Sanchala <rspathirana1017@gmail.com>
2025-04-15 20:20:45 +0530

    Add the styles for navBar

---
+++ src/styles/navBar.ts
@@ -0,0 +1,58 @@
+import { StyleSheet } from 'react-native';
+
+const navBarStyles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '70%',
+    height: '100%',
+    backgroundColor: 'black',
+    zIndex: 10, // ensures it's above other content
+  },
+
+  header: {
+    backgroundColor: '#3B0A75', 
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+
+  logo: {
+    width: 100,
+    height: 100,
+    marginRight: 10,
+    resizeMode: 'contain',
+  },
+
+  appName: {
+    color: 'white',
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
+
+  navBar: {
+    paddingVertical: 30,
+    paddingHorizontal: 20,
+  },
+
+  navItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 30,
+  },
+
+  icon: {
+    width: 24,
+    height: 24,
+    marginRight: 15,
+    resizeMode: 'contain',
+  },
+
+  navText: {
+    fontSize: 18,
+    color: 'white',
+    fontWeight: '500',
+  },
+});
+
+export default navBarStyles;
