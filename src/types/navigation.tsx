30f3732ce0691eb01a7664b07c8b33d817cee047
Rasadhi Sanchala <rspathirana1017@gmail.com>
2025-04-16 07:04:40 +0530

    Added  OngoingTask: undefined; CompletedTask: undefined;

--- src/types/navigation.tsx
+++ src/types/navigation.tsx
@@ -2,5 +2,8 @@ export type RootStackParamList = {
     Loading: undefined;//These screens don’t need any params. You can just navigate to them straight
     Home: undefined;
     AddTask: undefined; 
+    OngoingTask: undefined;
+    CompletedTask: undefined;
+    
   };
   
