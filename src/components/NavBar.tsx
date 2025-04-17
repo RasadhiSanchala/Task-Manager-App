1f2de2fe95e1d14f10d066bb1e829ea87ad67ec0
Rasadhi Sanchala <rspathirana1017@gmail.com>
2025-04-17 14:38:50 +0530

    Adjust as fomat doc

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -18,15 +18,14 @@ const NavBar: React.FC<NavBarProps> = ({ visible, onClose }) => {
   return (
     <TouchableWithoutFeedback onPress={onClose}>
       <View style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, zIndex: 9 }}>
-        {/* Nav bar part */}
+     
         <TouchableWithoutFeedback onPress={() => {}}>
           <View style={styles.container}>
-            {/* Header */}
+     
             <View style={styles.header}>
               <Image source={require('../assets/logo.png')} style={styles.logo} />
             </View>
 
-            {/* Menu items */}
             <View style={styles.navBar}>
               <TouchableOpacity
                 style={styles.navItem}
